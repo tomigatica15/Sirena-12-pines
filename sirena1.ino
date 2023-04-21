15682ae7c8bccfceaed9325053a9daa96efbbc71
Tomi <83985722+tomigatica15@users.noreply.github.com>
2023-04-21 13:15:18 -0300

    Add files via upload

--- sirena1.ino
+++ sirena1.ino
@@ -38,50 +38,20 @@ void simulacion() {                     // Crea una función con el nombre "simu
   }
 }
 
-//                                                                       //
-// Escalera, prende de arriba para abajo y se apaga de abajo para arriba //
-//                                                                       //
-
-void simulacion2() {
-  for (int led = 2; led <= 14; led++) {
-    digitalWrite(led, HIGH);
-    off();
-  }
-  for (int led = 14; led >= 2; led--) {
-    digitalWrite(led, LOW);
-    off();
-  }
-}
-
-//                                                                       //
-// Escalera, prende de abajo para arriba y se apaga de abajo para arriba //
-//                                                                       //
-
-void simulacion3() {
-  for (int led = 14; led >= 2; led--) {
-    digitalWrite(led, HIGH);
-    off();
-  }
-  for (int led = 14; led >= 2; led--) {
-    digitalWrite(led, LOW);
-    off();
-  }
-}
-
 //                                                               //
 // se prende adentro para fuera y se apaga de adentro para fuera //
 //                                                               //
 
-void simulacion4() {
+void simulacion2() {
   for (int led = 7, led2 = 8; led >= 2 && led2 <= 13; led--, led2++) {
-    digitalWrite(led, HIGH);   // Enciende el LED en el pin "i".
-    digitalWrite(led2, HIGH);  // Enciende el LED en el pin "i".
-    off();                     // Apaga el LED en el pin 2.
+    digitalWrite(led, HIGH);   // Enciende los leds declarados en "led".
+    digitalWrite(led2, HIGH);  // Enciende los leds declarados en "led2".
+    off();                     // Llama a la función "off".
   }
   for (int led = 7, led2 = 8; led >= 2 && led2 <= 13; led--, led2++) {
-    digitalWrite(led, LOW);   // Enciende el LED en el pin "i".
-    digitalWrite(led2, LOW);  // Enciende el LED en el pin "i".
-    off();                    // Apaga el LED en el pin 2.
+    digitalWrite(led, LOW);   // Apaga los LEDs declarados en "led".
+    digitalWrite(led2, LOW);  // Apaga los LEDs declarados en "led2".
+    off();                    // Llama a la función "off".
   }
 }
 
@@ -89,16 +59,16 @@ void simulacion4() {
 // se prende fuera para adentro y se apaga fuera para adentro //
 //                                                            //
 
-void simulacion5() {
+void simulacion3() {
   for (int led = 2, led2 = 13; led <= 7 && led2 >= 8; led++, led2--) {
-    digitalWrite(led, HIGH);   // Enciende el LED en el pin "led2".
-    digitalWrite(led2, HIGH);  // Enciende el LED en el pin "i".
-    off();                     // Apaga el LED en el pin 2.
+    digitalWrite(led, HIGH);   // Enciende los LEDs declarados en "led".
+    digitalWrite(led2, HIGH);  // Enciende los LEDs declarados en "led2".
+    off();                     // Llama a la función "off".
   }
   for (int led = 2, led2 = 13; led <= 7 && led2 >= 8; led++, led2--) {
-    digitalWrite(led, LOW);   // Enciende el LED en el pin "i".
-    digitalWrite(led2, LOW);  // Enciende el LED en el pin "i".
-    off();                    // Apaga el LED en el pin 2.
+    digitalWrite(led, LOW);   // Apaga los LEDs declarados en "led".
+    digitalWrite(led2, LOW);  // Apaga los LEDs declarados en "led2".
+    off();                    // Llama a la función "off".
   }
 }
 
@@ -106,16 +76,16 @@ void simulacion5() {
 // se prende fuera para adentro y se apaga adentro para fuera //
 //                                                            //
 
-void simulacion6() {
+void simulacion4() {
   for (int led = 2, led2 = 13; led <= 7 && led2 >= 8; led++, led2--) {
-    digitalWrite(led, HIGH);   // Enciende el LED en el pin "led2".
-    digitalWrite(led2, HIGH);  // Enciende el LED en el pin "i".
-    off();                     // Apaga el LED en el pin 2.
+    digitalWrite(led, HIGH);   // Enciende los LED declarados en "led".
+    digitalWrite(led2, HIGH);  // Enciende los LEDs declarados en "led2".
+    off();                     // Llama a la función "off".
   }
   for (int led = 7, led2 = 8; led >= 2 && led2 <= 13; led--, led2++) {
-    digitalWrite(led, LOW);   // Enciende el LED en el pin "led2".
-    digitalWrite(led2, LOW);  // Enciende el LED en el pin "i".
-    off();                    // Apaga el LED en el pin 2.
+    digitalWrite(led, LOW);   // Apagalosl LED declarados en "led".
+    digitalWrite(led2, LOW);  // Apaga los LEDs declarados en "led2".
+    off();                    // Llama a la función "off".
   }
 }
 
@@ -123,16 +93,16 @@ void simulacion6() {
 // se prende adentro para fuera y se apaga fuera para adentro //
 //                                                            //
 
-void simulacion7() {
+void simulacion5() {
   for (int led = 7, led2 = 8; led >= 2 && led2 <= 13; led--, led2++) {
-    digitalWrite(led, HIGH);   // Enciende el LED en el pin "led2".
-    digitalWrite(led2, HIGH);  // Enciende el LED en el pin "led2".
-    off();                     // Apaga el LED en el pin 2.
+    digitalWrite(led, HIGH);   // Enciende los LED declarados en "led".
+    digitalWrite(led2, HIGH);  // Enciende los LED declarados en "led2".
+    off();                     // Llama a la función "off".
   }
   for (int led = 2, led2 = 13; led <= 7 && led2 >= 8; led++, led2--) {
-    digitalWrite(led, LOW);   // Enciende el LED en el pin "led2".
-    digitalWrite(led2, LOW);  // Enciende el LED en el pin "i".
-    off();                    // Apaga el LED en el pin 2.
+    digitalWrite(led, LOW);   // Apagalosl LED declarados en "led".
+    digitalWrite(led2, LOW);  // Apaga los LEDs declarados en "led2".
+    off();                    // Llama a la función "off".
   }
 }
 
@@ -141,15 +111,19 @@ void simulacion7() {
 //                                 //
 
 void end() {
-  off();                                 // Llama a la función "off2".
+  off();                                 // Llama a la función "off".
   for (int led = 2; led <= 14; led++) {  // Activa los pines del 8 al 14.
     digitalWrite(led, HIGH);             // Enciende los pines.
   }
-  off();                                 // Llama a la función "off2".
+  off();                                 // Llama a la función "off".
   for (int led = 2; led <= 14; led++) {  // Activa los pines del 8 al 14.
     digitalWrite(led, LOW);              // Apaga los pines.
   }
 }
+
+//                    //
+// Funciónes en bucle //
+//                    //
 
 void loop() {  // Bucle llamando a todas las funciones escritas anteriormente
   simulacion();
@@ -157,8 +131,6 @@ void loop() {  // Bucle llamando a todas las funciones escritas anteriormente
   simulacion3();
   simulacion4();
   simulacion5();
-  simulacion6();
-  simulacion7();
   end();
   end();
 }
